79f9e9b136b5ae01dd0a8fa49557eef6b5f56d16
maferfan <matheusferrazza@gmail.com>
2023-03-09 14:36:21 -0300

    criação da página devteam

---
+++ scripts/devteam.js
@@ -0,0 +1,406 @@
+
+
+var users = [
+  {
+    titulo: "Alex miranda bastos",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Allysson Costa",
+    descricao: "https://github.com/AllyCosta07",
+    imagem: "https://avatars.githubusercontent.com/u/113447567?v=4",
+  },
+  {
+    titulo: "Ana Paula Goettens",
+    descricao: "https://github.com/anaschwaab",
+    imagem: "https://avatars.githubusercontent.com/u/55794309?v=4",
+  },
+  {
+    titulo: "Ana Victoria",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Anderson Zamboni",
+    descricao: "https://github.com/ZamboniA",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Bruna Faria",
+    descricao: "https://github.com/Brunafariia",
+    imagem: "https://avatars.githubusercontent.com/u/125146097?v=4",
+  },
+  {
+    titulo: "Caio César",
+    descricao: "https://github.com/caioCesarLDias",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Caique Gomes",
+    descricao: "https://github.com/caiquegomessa",
+    imagem: "https://avatars.githubusercontent.com/u/125289836?v=4",
+  },
+  {
+    titulo: "Claudio Filho",
+    descricao: "https://github.com/ClaudioFGMartins",
+    imagem: "https://avatars.githubusercontent.com/u/125303874?v=4",
+  },
+  {
+    titulo: "Cristiano Oliveira",
+    descricao: "https://github.com/cristianoans",
+    imagem: "https://avatars.githubusercontent.com/u/79390447?v=4",
+  },
+  {
+    titulo: "Cristina Costa",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Daniela Lopes",
+    descricao: "https://github.com/DGuilhoto",
+    imagem: "https://avatars.githubusercontent.com/u/125145859?v=4",
+  },
+  {
+    titulo: "Danilo Araújo de Oliveira",
+    descricao: "https://github.com/oliverdn95",
+    imagem: "https://avatars.githubusercontent.com/u/117095640?v=4",
+  },
+  {
+    titulo: "Dayse Guedes",
+    descricao: "https://github.com/daydocx",
+    imagem: "https://avatars.githubusercontent.com/u/120271038?v=4",
+  },
+  {
+    titulo: "Diego Alves",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Diego Arruda",
+    descricao: "https://github.com/DiegoArruda",
+    imagem: "https://avatars.githubusercontent.com/u/24374203?v=4",
+  },
+  {
+    titulo: "Eurico Oliveira",
+    descricao: "https://github.com/EuricoOliveira",
+    imagem: "https://avatars.githubusercontent.com/u/125271825?v=4",
+  },
+  {
+    titulo: "Fernanda Franco",
+    descricao: "https://github.com/Fernanda-Franco",
+    imagem: "https://avatars.githubusercontent.com/u/125104687?v=4",
+  },
+  {
+    titulo: "Gabriel",
+    descricao: "https://github.com/gabdiniz",
+    imagem: "https://avatars.githubusercontent.com/u/115419806?v=4",
+  },
+  {
+    titulo: "Gabriel Camilo",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Gabriela Aragao",
+    descricao: "https://github.com/gabiaraga",
+    imagem: "https://avatars.githubusercontent.com/u/98778064?v=4",
+  },
+  {
+    titulo: "Gabriela Mazetto",
+    descricao: "https://github.com/gabimazetto",
+    imagem: "https://avatars.githubusercontent.com/u/125303672?v=4",
+  },
+  {
+    titulo: "Gabriela Ruas",
+    descricao: "https://github.com/gabyruas",
+    imagem: "https://avatars.githubusercontent.com/u/114955881?v=4",
+  },
+  {
+    titulo: "Guilherme Thomazini",
+    descricao: "https://github.com/thomazinig",
+    imagem: "https://avatars.githubusercontent.com/u/77416405?v=4",
+  },
+  {
+    titulo: "Gustavo Leonardi",
+    descricao: "https://github.com/GuLeonardi",
+    imagem: "https://avatars.githubusercontent.com/u/59261242?v=4",
+  },
+  {
+    titulo: "Ítalo Baracho",
+    descricao: "https://github.com/italobaracho",
+    imagem: "https://avatars.githubusercontent.com/u/94207768?v=4",
+  },
+  {
+    titulo: "Jamilson Pestana",
+    descricao: "https://github.com/devruso",
+    imagem: "https://avatars.githubusercontent.com/u/70742347?v=4",
+  },
+  {
+    titulo: "Jessica Venancio",
+    descricao: "https://github.com/jessicavenancio",
+    imagem: "https://avatars.githubusercontent.com/u/120992355?v=4",
+  },
+  {
+    titulo: "João Lima",
+    descricao: "https://github.com/eujoaolima",
+    imagem: "https://avatars.githubusercontent.com/u/106890966?v=4",
+  },
+  {
+    titulo: "João Pedro Fernandes",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Jonatas Vieira",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "José Ailton Júnior",
+    descricao: "https://github.com/joseailtonjr",
+    imagem: "https://avatars.githubusercontent.com/u/107266472?v=4",
+  },
+  {
+    titulo: "Jose Augusto da Silva Farias",
+    descricao: "https://github.com/joseaugustoss",
+    imagem: "https://avatars.githubusercontent.com/u/13836536?v=4",
+  },
+  {
+    titulo: "Julia Gascho",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Juliana Andrade",
+    descricao: "https://github.com/andradeju",
+    imagem: "https://avatars.githubusercontent.com/u/90662162?v=4",
+  },
+  {
+    titulo: "Juliana Barros",
+    descricao: "https://github.com/Julybarros",
+    imagem: "https://avatars.githubusercontent.com/u/125303873?v=4",
+  },
+  {
+    titulo: "Julio Cesar",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Jurineide Souza",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Lais Belo",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Laura Xavier",
+    descricao: "https://github.com/lauraxavier",
+    imagem: "https://avatars.githubusercontent.com/u/48969085?v=4",
+  },
+  {
+    titulo: "Leonardo Pacheco",
+    descricao: "https://github.com/leopachecof",
+    imagem: "https://avatars.githubusercontent.com/u/119996107?v=4",
+  },
+  {
+    titulo: "Luana Souza",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Lucas Damaso",
+    descricao: "https://github.com/LuhD1",
+    imagem: "https://avatars.githubusercontent.com/u/40504367?v=4",
+  },
+  {
+    titulo: "Lucas Leão",
+    descricao: "https://github.com/magalhes",
+    imagem: "https://avatars.githubusercontent.com/u/111883029?v=4",
+  },
+  {
+    titulo: "Lucas Velsch",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Luciana Monteiro",
+    descricao: "https://github.com/Luciana-mon",
+    imagem: "https://avatars.githubusercontent.com/u/61600055?v=4",
+  },
+  {
+    titulo: "Luis Guedes",
+    descricao: "https://github.com/luisgued3s",
+    imagem: "https://avatars.githubusercontent.com/u/104780405?v=4",
+  },
+  {
+    titulo: "Luiz Eduardo Testa Santos",
+    descricao: "https://github.com/luizeduardotesta",
+    imagem: "https://avatars.githubusercontent.com/u/94090632?v=4",
+  },
+  {
+    titulo: "Marcos dos Santos",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Mateus Laureano",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Matheus Ferrazza",
+    descricao: "https://github.com/maferfan",
+    imagem: "https://avatars.githubusercontent.com/u/125100814?v=4",
+  },
+  {
+    titulo: "Matheus Meira",
+    descricao: "https://github.com/MatheusMeira-Dev",
+    imagem: "https://avatars.githubusercontent.com/u/125158417?v=4",
+  },
+  {
+    titulo: "Mikael Meira",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Milena Carolina",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Monnicy T",
+    descricao: "https://github.com/Monni-Tonon",
+    imagem: "https://avatars.githubusercontent.com/u/96590402?v=4",
+  },
+  {
+    titulo: "Nai Abreu",
+    descricao: "https://github.com/UtescherIntrieri/",
+    imagem: "https://avatars.githubusercontent.com/u/60987778?v=4",
+  },
+  {
+    titulo: "Nicolas Moreira Neves",
+    descricao: "https://github.com/Nickmn7",
+    imagem: "https://avatars.githubusercontent.com/u/125204234?v=4",
+  },
+  {
+    titulo: "Nilson Mazurchi",
+    descricao: "https://github.com/nilsonmazurchi",
+    imagem: "https://avatars.githubusercontent.com/u/55157982?v=4",
+  },
+  {
+    titulo: "Rafael Menezes",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Raissa Kimberly",
+    descricao: "https://github.com/kimbiess",
+    imagem: "https://avatars.githubusercontent.com/u/113555561?v=4",
+  },
+  {
+    titulo: "Raphael Gomes Faria",
+    descricao: "https://github.com/Raphaeldev84",
+    imagem: "https://avatars.githubusercontent.com/u/125095194?v=4",
+  },
+  {
+    titulo: "Rebeca Carvalho",
+    descricao: "https://github.com/rebecacarvalhoaraujo",
+    imagem: "https://avatars.githubusercontent.com/u/112990922?v=4",
+  },
+  {
+    titulo: "Renata Rabelo",
+    descricao: "https://github.com/RenataRCPedreschi",
+    imagem: "https://avatars.githubusercontent.com/u/99511265?v=4",
+  },
+  {
+    titulo: "Ricardo Sousa",
+    descricao: "https://github.com/ricardodev1605",
+    imagem: "https://avatars.githubusercontent.com/u/125304043?v=4",
+  },
+  {
+    titulo: "Sabrina Lopes Oliveira",
+    descricao: "https://github.com/sasalopes",
+    imagem: "https://avatars.githubusercontent.com/u/88273961?v=4",
+  },
+  {
+    titulo: "Sandra Silva",
+    descricao: "https://github.com/SandramSilva",
+    imagem: "https://avatars.githubusercontent.com/u/125217859?v=4",
+  },
+  {
+    titulo: "Stephanie Ludovico",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Stephany Canuto",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Tamires Sanchez",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Thomas Almeida",
+    descricao: "https://github.com/tgo-mas",
+    imagem: "https://avatars.githubusercontent.com/u/99335340?v=4",
+  },
+  {
+    titulo: "Vanessa Neves",
+    descricao: "https://github.com/vanessaneves-dev",
+    imagem: "https://avatars.githubusercontent.com/u/113128675?v=4",
+  },
+  {
+    titulo: "Victoria Scapini",
+    descricao: "https://github.com/VicScapini",
+    imagem: "https://avatars.githubusercontent.com/u/125276469?v=4",
+  },
+  {
+    titulo: "Vitor Santana",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Wanessa Dantas",
+    descricao: "",
+    imagem: "https://avatars.githubusercontent.com/u/9919?s=460&v=4",
+  },
+  {
+    titulo: "Yuri Schuery",
+    descricao: "https://github.com/souzaschuery",
+    imagem: "https://avatars.githubusercontent.com/u/125303833?v=4",
+  },
+];
+
+let allUsers = document.getElementById("items");
+
+
+for (let i = 0; i < users.length; i++) {
+  const obj = users[i];
+    if (i === 0) {
+        allUsers.innerHTML += `<div class="carousel-item active">
+        <div class="userName">
+            <div class="avatar">
+                <img src="${obj.imagem}" class="d-block"/>
+            </div>
+            <h3 class="stundentName">${obj.titulo}</h3>
+            <a href="${obj.descricao}" target="blank">GitHub</a>
+        </div>
+    </div>`;
+    }else{
+        allUsers.innerHTML += `<div class="carousel-item">
+        <div class="userName">
+            <img src="${obj.imagem}" class="d-block avatar" />
+            <h3 class="stundentName">${obj.titulo}</h3>
+            <a href="${obj.descricao}" target="blank">GitHub</a>
+        </div>
+    </div>`;
+    }
+  
+}

ea2cec957c8f072e0b416b34626d5451653624ae
cristianoans <cristianoans@hotmail.com>
2023-03-10 09:33:07 -0300

    criada a validação do form de recupreção de senha

--- scripts/scriptRecuperarSenhaModal.js
+++ scripts/scriptRecuperarSenhaModal.js
@@ -1,8 +1,41 @@
 /* Botão de recuperação de senha */
-const buttonEnviar = document.getElementById("btnRecuperarSenha");
+let buttonEnviar = document.getElementById("btnRecuperarSenha");
+let recuperarUsuario = document.getElementById('recuperarUsuario');
+let recuperarEmail = document.getElementById('recuperarEmail');
+let staticBackdrop = document.getElementById('staticBackdrop');
+let fecharModal = document.getElementById('fecharModal');
+
+
 
 buttonEnviar.addEventListener("click", () => {
-  const modalEmail = document.getElementById("recuperarEmail").value;
-  const paragrafoModal = document.getElementById("paragrafoModal");
-  paragrafoModal.innerText = `Verificar a caixa de entrada do ${modalEmail}`;
+  let valid = recuperarEmail.checkValidity()
+  if (recuperarUsuario.value !== '' && valid === true) {
+    recuperarUsuario.className = 'form-control is-valid';
+    recuperarEmail.className = 'form-control is-valid';
+
+    abrirModal();
+
+  } else if (valid === false && recuperarUsuario.value !== '') {
+    recuperarEmail.className = 'form-control is-invalid';
+    recuperarUsuario.className = 'form-control is-valid';
+  } else if (valid === true && recuperarUsuario.value === '') {
+    recuperarEmail.className = 'form-control is-valid';
+    recuperarUsuario.className = 'form-control is-invalid';
+  } else {
+    recuperarUsuario.className = 'form-control is-invalid';
+    recuperarEmail.className = 'form-control is-invalid';
+  }
+
+});
+
+let abrirModal = () => {
+  let paragrafoModal = document.getElementById("paragrafoModal");
+  paragrafoModal.innerText = `Verificar a caixa de entrada do ${recuperarEmail.value}`;
+  staticBackdrop.classList.add("show");
+  staticBackdrop.style.display = "block";
+}
+
+fecharModal.addEventListener("click", () => {
+  staticBackdrop.classList.remove("show");
+  staticBackdrop.style.display = "none";
 });
